086645f237a3ac5b442213ed111e994bb938b968
Mark de Haan <mark.dehaan@inholland.nl>
2022-09-13 17:04:49 +0200

    Removed unnecessary references

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module nl.inholland.fxtodoapp {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.kordamp.bootstrapfx.core;
-
     opens nl.inholland.fxtodoapp to javafx.fxml;
     exports nl.inholland.fxtodoapp;
 }
